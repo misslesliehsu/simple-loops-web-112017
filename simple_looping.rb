01de56b685390f2b8e136a8af08fd367ede186cf
Leslie Hsu <chuckleberries@hotmail.com>
2017-11-10 22:11:33 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,11 +33,11 @@ def until_iterator(number_of_times)
   until counter == number_of_times do
     puts phrase
     counter += 1
-end  
+end
 end
 
 def for_iterator(number_of_times)
-  
+
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for times in (0..number_of_times)
     puts phrase
